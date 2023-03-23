a5e58237b3210200b02c5030318fee3c8f2b010d
Half_dead <84064967+bsws5132@users.noreply.github.com>
2023-03-23 11:27:45 +0900

    Create  Emergency_BackUp_MyHomework.cpp
    과제 내야하는거 잠시 급히 백업용

---
+++ Emergency_BackUp_MyHomework.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "*" << endl;
+	cout << "**" << endl;
+	cout << "***" << endl;
+
+	cout << endl;
+
+	cout << "*" << endl;
+	cout << "**" << endl;
+	cout << "***" << endl;
+	cout << "**" << endl;
+	cout << "*" << endl;
+
+	cout << "H	H" << endl;
+	cout << "H	H" << endl;
+	cout << "H	H" << endl;
+	cout << "H	H" << endl;
+	cout << "HHHHHHHHH" << endl;
+	cout << "H	H" << endl;
+	cout << "H	H" << endl;
+	cout << "H	H" << endl;
+	cout << "H	H" << endl;
+
+	cout << false << " " << 'A' << " " << "hello" << endl;
+	cout << 23142 << " " << 12897234L << endl;
+	cout << 245.78F << 114.782 << " " << 2.051L;
+
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	int x = 4;
+	cout << (x * 3) * 5 << endl;
+	cout << x * 3 * 5 << endl << endl;
+	cout << 12 / (x + 2) << endl;
+	cout << 12 / x + 2;
+
+
+	return 0;
+}
